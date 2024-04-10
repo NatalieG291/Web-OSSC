e2f4f444403247e9f01fc1809b6122c61eab1cae
Soporte ossc <soporte@OSSCmexico.onmicrosoft.com>
2024-04-09 19:23:08 -0600

    Cambios del boton de navegacion

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var home = '<i class="bi bi-house"></i>'
 function nameBtn(){
-    //document.getElementById("navButton").innerHTML = home + " " + document.title;
+    document.getElementById("navButton").innerHTML = home + " " + document.title;
 }
 function openNav() {
   document.getElementById("mySidebar").style.height = "50%";
   document.getElementById("mySidebar").style.width = "12%";
-  //document.getElementById("navButton").innerHTML = home + " " + document.title;
+    document.getElementById("navButton").innerHTML = home + " " + document.title;
 }
 
 document.getElementById("mySidebar").addEventListener("mouseleave", function() {
   document.getElementById("mySidebar").style.height = "7%";
-  document.getElementById("mySidebar").style.width = "12%";
-//   document.getElementById("navButton").style.left = "50px";
-  //document.getElementById("navButton").innerHTML = home + " " + document.title;
+  document.getElementById("mySidebar").style.width = "7%";
+  document.getElementById("navButton").style.left = "50px";
+    document.getElementById("navButton").innerHTML = home + " " + document.title;
 });
