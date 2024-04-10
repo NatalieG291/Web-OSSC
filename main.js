27ad683427dfebd353173b7fbfbc12371f591708
Soporte ossc <soporte@OSSCmexico.onmicrosoft.com>
2024-04-10 11:36:13 -0600

    Boton menu corregido (pagina actual prueba)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var home = '<i class="bi bi-house"></i>'
 function nameBtn(){
-    document.getElementById("navButton").innerHTML = home + " " + document.title;
+    document.getElementById("navButton").innerHTML = home + " Inicio"; //+ document.title;
 }
 function openNav() {
   document.getElementById("mySidebar").style.height = "50%";
@@ -11,6 +11,6 @@ function openNav() {
 document.getElementById("mySidebar").addEventListener("mouseleave", function() {
   document.getElementById("mySidebar").style.height = "7%";
   document.getElementById("mySidebar").style.width = "7%";
-  document.getElementById("navButton").style.left = "50px";
-    document.getElementById("navButton").innerHTML = home + " " + document.title;
+    document.getElementById("navButton").style.left = "50px";
+    document.getElementById("navButton").innerHTML = home + " Inicio"; //+ document.title;
 });
