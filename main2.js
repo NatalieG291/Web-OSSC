11d21e3aa08cd0d088316e06596887ebfc1dbac2
Soporte <Soporte@DESKTOP-NISTK00>
2024-04-19 12:49:56 -0600

    Ajuste de menu responsivo para medidas mas pequeñas.

--- main2.js
+++ main2.js
@@ -1,3 +1,8 @@
+var wBrowser = window.outerWidth;
+var hBrowser = window.outerHeight;
+
+console.log("W: " + wBrowser + "H: " + hBrowser);
+
 document.getElementById("btn").addEventListener("mouseenter", function mostrarT() {
     document.getElementById("itm").style.transition = "0.8s";
     document.getElementById("itm").style.opacity = "1";
@@ -6,3 +11,9 @@ document.getElementById("btn").addEventListener("mouseleave", function ocultarT(
     document.getElementById("itm").style.transition = "0.2s";
     document.getElementById("itm").style.opacity = "0";
 });
+
+function resizeW() {
+    wBrowser = window.outerWidth;
+    hBrowser = window.outerHeight;
+    console.log("W: " + wBrowser + "H: " + hBrowser);
+}
