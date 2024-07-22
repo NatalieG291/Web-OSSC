5613af74573a21cfe9df89a8c7b64903e4d02a8a
Soporte ossc <soporte@OSSCmexico.onmicrosoft.com>
2024-07-22 15:08:15 -0600

    Funcionalidad formulario de contacto

---
+++ acciones/EnviaEmail.php
@@ -0,0 +1,99 @@
+<?php
+    session_start();
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    
+    /* Exception class. */
+    require '../mail/src/Exception.php';
+    
+    /* The main PHPMailer class. */
+    require '../mail/src/PHPMailer.php';
+    
+    /* SMTP class, needed if you want to use SMTP. */
+    require '../mail/src/SMTP.php';
+
+    require 'config.php';
+
+	$respuesta = array();
+
+    $Asunto = $_REQUEST['Asunto'];
+	$Nombre = $_REQUEST['Nombre'];
+	$Email = $_REQUEST['Email'];
+	$Mensaje = $_REQUEST['mensaje'];
+    $mail = new PHPMailer(true);
+    $mail->IsSMTP();
+    ob_start();
+    ?>
+        <!DOCTYPE HTML>
+        <html>
+            <head>
+                <style>
+                    a
+                    {
+                        background-color: RGBA(74, 112, 89);
+                        padding: 15px;
+                        text-decoration: none;
+                        color: gray;
+                        font-family: segoe ui;
+                    }
+                    h1, h2, p, span
+                    {
+                        font-family: segoe UI;
+                    }
+                </style>
+            </head>
+            <body>
+                <center>
+                <h1><?php echo $Asunto; ?></h1>
+                <p>El usuario <b><?php echo $Nombre; ?> </b>solicito comunicarse al siguiente correo: <?php echo $Email; ?>.</p>
+				<h2>Mensaje:</h2>
+				<p><?php echo $Mensaje; ?></p>
+                </center>
+            </body>
+        </html>
+    <?php
+    $content = ob_get_contents();
+    ob_end_clean();
+    @file_put_contents($cacheFile,$content);
+    //echo $content;
+    try
+    {
+        $mail->CharSet = 'UTF-8';
+        $mail->Host = $ServidorSMTP;
+        $mail->SMTPDebug = $Debuggear;
+        $mail->SMTPSecure = $Seguridad;
+        $mail->SMTPAuth = $Autenticar;
+        $mail->Port = $Puerto;
+        $mail->Username = $Correo;
+        $mail->Password = $Contraseña;
+
+        $emails = 'natali11.ngs@gmail.com';
+        array_map(array($mail, 'AddAddress'), explode(';', $emails));
+
+        $mail->SetFrom($Correo, $NombreMostrar);
+        $mail->Subject = $Asunto;
+        $mail->AltBody = '<b>Para ver el mensaje, por favor use un visor HTML compatible.</b>'; // 
+        $mail->MsgHTML($content);
+        $mail->Send();
+
+        $respuesta['error'] = "no";
+        $respuesta['msg'] = "El correo se envio correctamente";
+    }
+    catch(phpmailerException $e)
+    {
+        $respuesta['error'] = "si";
+        $respuesta['msg'] = "No se envio el correo electronico";
+
+        //echo $e;
+    }
+    catch(Exception $e)
+    {
+        $respuesta['error'] = "si";
+        $respuesta['msg'] = "No se envio el correo electronico (ex) <br>".$e."";
+
+        echo $e;
+    }
+
+	echo json_encode($respuesta);
+?>
---
+++ acciones/config.php
@@ -0,0 +1,14 @@
+<?php
+    //Configuración del servidor
+    $ServidorSMTP = 'smtp.office365.com';
+    $Debuggear = false;
+    $Seguridad = 'tls';
+    $Autenticar = true;
+    $Puerto = '587';
+    $Correo = 'recibonomina@sbgroup.com.mx';
+    $Contraseña = 'Sbl$Nom2023#';
+
+    //Configuración del correo
+    $Asunto = 'Solicitud de contacto';
+    $NombreMostrar = 'web OSSC';
+?>
