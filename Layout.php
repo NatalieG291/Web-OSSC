b1db0e4263690a7eb28960b79f2cc8e47a7d52b4
Soporte ossc <soporte@OSSCmexico.onmicrosoft.com>
2024-08-28 10:50:30 -0600

    Layout base para paginas, secciones de la pagina agregadas independientemente

---
+++ Layout.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <link rel="stylesheet" href="styles/menu.css" />
+    <link rel="stylesheet" href="styles/form.css" />
+    <link rel="stylesheet" href="styles/fonts.css" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+</head>
+<body>
+    <?php include("secciones/menu.php"); ?>
+
+    <!-- CONTENIDO -->
+
+    <?php include("secciones/email.php"); ?>
+    <?php include("secciones/footer.php"); ?>
+
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <script type="text/javascript" src="lib/form.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+            integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+            crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</body>
+</html>
---
+++ Secciones/email.php
@@ -0,0 +1,56 @@
+﻿    <div style="display: block; visibility: visible;" id="formsend">
+        <div class="formsend js-formsend" id="formOpen">
+            <div class="formsend-underlay"></div> <a class="formsend__trigger text-replace form" id="btnOpenForm"></a>
+            <div class="formsend__content">
+                <div class="formsend__layout">
+                    <header class="formsend__header" style="zoom: 1;">
+                        <h2 style="text-align: left;"> <span>Contactanos</span></h2>
+                        <div class="formsend-account" style="display: flex; align-items: center;">
+                            <span class="formsend__close" id="btnOpenForm2"></span>
+                        </div>
+                    </header>
+                    <div id="formsend-body" class="formsend__body" style="zoom: 1;">
+                        <div>
+                            <div class="formsend-option formsend-toggle formsend-form"
+                                 style="display: block; zoom: 1.2;">
+                                <div style="margin-bottom: 10px;">
+                                    <div class="formsend-label">
+                                        Nombre <span class="formsend-required">*</span>
+                                    </div> <input type="text" id="txtNombre" class="formsend-input" required>
+                                </div>
+                                <div style="margin-bottom: 10px;">
+                                    <div class="formsend-label">
+                                        Correo electronico <span class="formsend-required">*</span>
+                                    </div> <input type="email" id="txtEmail" class="formsend-input" required>
+                                </div>
+                                <div style="margin-bottom: 10px;">
+                                    <div class="formsend-label">
+                                        ¿Como podemos ayudarte? <span class="formsend-required">*</span>
+                                    </div> <textarea class="formsend-input"
+                                                     style="min-height: 80px; padding: 5px 10px;" id="txtDetalle" required></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <footer class="formsend__footer" style="font-size: 14px;">
+                        <a class="formsend__checkout"
+                           style="background-color: rgb(63, 156, 217);" id="btnEnviar" onclick="EnviaMensaje()">
+                            <div class="spinner-border visually-hidden" id="statusSpinner" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+                            <em id="status">
+                                <span id="txtStatus">Enviar</span> <svg viewBox="0 0 24 24" class="icon icon--sm">
+                                    <g fill="none" stroke="#FFF">
+                                        <line stroke-width="2" stroke-linecap="round" stroke-linejoin="round" x1="3"
+                                              y1="12" x2="21" y2="12"></line>
+                                        <polyline stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                  points="15,6 21,12 15,18 "></polyline>
+                                    </g>
+                                </svg>
+                            </em>
+                        </a>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </div>
