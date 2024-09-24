18e8da451c91abde487e0be146071b946170501a
menaxe20 <menaxe9@gmail.com>
2024-09-24 12:05:21 -0600

    Actualizacion Nosotros.

--- Secciones/nosotros.php
+++ Secciones/nosotros.php
@@ -1,4 +1,9 @@
-<div class="container-fluid full-width">
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container-fluid">
     <div class="row">
         <div class="col content-overlay d-flex justify-content-center align-items-center text-center">
             <p class="text-ossc-2 fontsize">Nosotros</p>
@@ -6,41 +11,56 @@
     </div>
     <div class="row">
         <div class="col d-flex justify-content-center text-center">
-            <h2 class="text-ossc-2">Somos un equipo <span class="text-ossc-1"><b>comprometido</b></span> con mejorar y
+            <p class="text-ossc-2 fontsize3">Somos un equipo <span class="text-ossc-1"><b>comprometido</b></span> con
+                mejorar y
                 optimizar la
                 administración de personal
                 en tu empresa. <br> Desde 2008, nos hemos dedicado a ofrecer <span
                     class="text-ossc-1"><b>soluciones</b></span> innovadoras y
                 fáciles de integrar, diseñadas para <br> simplificar y automatizar los procesos de nómina y recursos
                 humanos.
-            </h2>
+            </p>
         </div>
     </div>
-    <div class="row">
-        <div class="col d-flex justify-content-center align-items-center text-center">
-            <p class="text-ossc-2 fontsize">Somos <img src="/images/Logo_OSSC.png" alt="Logo" class="logos"></p>
+    <div class="row mt-0">
+        <div class="col d-flex justify-content-center text-center">
+            <h1 class="text-ossc-2 fontsize">Somos <img src="/images/Logo_OSSC.png" alt="Logo" class="logos"></h1>
         </div>
     </div>
-    <div class="row">
-        <div class="col d-flex justify-content-center align-items-center">
-            <img src="/images/cinta.png" alt="Ribbon" class="cinta">
+    <div class="row position-relative">
+        <div class="col position-relative">
+            <img src="/images/cinta.png" alt="cinta" class="cinta">
+        </div>
+
+        <!-- Primer Elemento -->
+        <div class="col-elemento position-absolute elemento-1 d-flex flex-column align-items-center text-center">
+            <img src="/images/c1.png" alt="c1" class="globos">
+            <p class="text-ossc-1 mt-0 fontsize2">Experiencia en desarrollo de Software</p>
+        </div>
+
+        <!-- Segundo Elemento -->
+        <div class="col-elemento position-absolute elemento-2 d-flex flex-column align-items-center text-center">
+            <img src="/images/c2.png" alt="c2" class="globos">
+            <p class="text-ossc-1 mt-0 fontsize2">Visión clara y objetiva</p>
+        </div>
+
+        <!-- Tercer Elemento -->
+        <div class="col-elemento position-absolute elemento-3 d-flex flex-column align-items-center text-center">
+            <img src="/images/c3.png" alt="c3" class="globos">
+            <p class="text-ossc-1 mt-0 fontsize2">Innovación y practicidad</p>
+        </div>
+
+        <!-- Cuarto Elemento -->
+        <div class="col-elemento position-absolute elemento-4 d-flex flex-column align-items-center text-center">
+            <img src="/images/c4.png" alt="c4" class="globos">
+            <p class="text-ossc-1 mt-0 fontsize2">Compromiso con nuestros clientes</p>
+        </div>
+
+        <!-- Quinto Elemento -->
+        <div class="col-elemento position-absolute elemento-5 d-flex flex-column align-items-center text-center">
+            <img src="/images/c5.png" alt="c5" class="globos">
+            <p class="text-ossc-1 mt-0 fontsize2">Orientación a resultados</p>
         </div>
     </div>
-    <div class="row mt-0 p-0">
-        <div class="col-2">
-            <p>Some Text</p>
-        </div>
-        <div class="col-2">
-            <p>Some Text</p>
-        </div>
-        <div class="col-2">
-            <p>Some Text</p>
-        </div>
-        <div class="col-2">
-            <p>Some Text</p>
-        </div>
-        <div class="col-2">
-            <p>Some Text</p>
-        </div>
-    </div>
+
 </div>
